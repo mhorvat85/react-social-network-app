7eeadebc9ae25e3e7b36746da7082c9b115703a9
Marko Horvat <horvat.marko2212@gmail.com>
2023-03-25 00:37:21 +0100

    html templates, client-side routing, composition

--- app/Main.js
+++ app/Main.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 
 const Main = () => {
-  return <div>Welcome to app!</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomeGuest />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 const root = ReactDOM.createRoot(document.querySelector("#app"));
