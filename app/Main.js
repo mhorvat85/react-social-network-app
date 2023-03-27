694dc922a688b7e616ce750bc67310c0e1246bf4
Marko Horvat <horvat.marko2212@gmail.com>
2023-03-28 00:13:46 +0200

    creating posts

--- app/Main.js
+++ app/Main.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Axios from "axios";
+Axios.defaults.baseURL = "http://localhost:8080";
 
 import Header from "./components/Header";
 import HomeGuest from "./components/HomeGuest";
@@ -8,15 +10,26 @@ import Home from "./components/Home";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Terms from "./components/Terms";
+import CreatePost from "./components/CreatePost";
+import ViewSinglePost from "./components/ViewSinglePost";
+import FlashMessages from "./components/FlashMessages";
 
 const Main = () => {
   const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("socialAppToken")));
+  const [flashMessages, setFlashMessages] = useState([]);
+
+  function addFlashMessage(msg) {
+    setFlashMessages((prev) => prev.concat(msg));
+  }
 
   return (
     <BrowserRouter>
+      <FlashMessages messages={flashMessages} />
       <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
         <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+        <Route path="/post/:id" element={<ViewSinglePost />} />
+        <Route path="/create-post" element={<CreatePost addFlashMessage={addFlashMessage} />} />
         <Route path="/about-us" element={<About />} />
         <Route path="/terms" element={<Terms />} />
       </Routes>
